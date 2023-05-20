24a00260a7efd20fa9b9b0dbd57373c12c7aaae2
wahyusattriana <wahyusattriana@gmail.com>
2023-05-20 12:13:53 +0800

    finalizing

--- src/pages/Checkouting.jsx
+++ src/pages/Checkouting.jsx
@@ -2,7 +2,7 @@ import { ShopContext } from "../context/shop-context"
 import { useContext } from "react"
 import './Checkouting.css'
 import { PRODUCTS } from "../products"
-import { CartItem } from "./cart/cart-item-2"
+import { CartItemFinal } from "./cart/cart-item-2"
 
 const convertRupiah = require('rupiah-format')
 
@@ -10,23 +10,30 @@ const Checkouting = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext) /** cartItems untuk mendapatkan data item apa saja yang ada di keranjang */
   const totalAmount = convertRupiah.convert(getTotalCartAmount()) /** totalAmount adalah hasil harga yang sudah di convert ke rupiah */
 
-  
+  PRODUCTS.map((product) => {
+    if (cartItems[product.id] !== 0) {
+      console.log(product)
+    }
+  })
+
   return (
     <div className="Checkouting">
-       <div>
+        <div>
                 <h1>Daftar Belanja</h1>
             </div>
-            <div className="cartItems">
+            <div className="cartItemsFinalBanget">
                 {PRODUCTS.map((product) => {
                     if (cartItems[product.id] !== 0) {
-                        return <CartItem data={product}/>
+                        return <CartItemFinal data={product}/>
                     }
                 })}
                 <p className="totalAmount">Total: {totalAmount}</p>
-                <p>INGAT SCREENSHOT PESANAN SEBELUM CHECKOUT!</p>
+                <p className="disclaimer"><b>Screenshot halaman ini lalu tunjukkan ke admin!</b></p>
             </div>
       <h1>{getTotalCartAmount}</h1>
-      <button>CHECKOUT</button>
+      <a href="https://wa.me/6287855130964/?text=Tunjukkan%20screenshot%20halaman%20yang%20tadi%20kamu%20isi%20yaa%20%F0%9F%A4%97%F0%9F%A5%B0%0AFix%20order%20%3D%20No%20hit%20and%20run!%20%0A%0AFormat%20order%F0%9F%98%8A%F0%9F%91%87%0A%0ANama%2FKelas%20%3A%20%0ANo%20Hp%2FWA%20%3A%20%0ALokasi%20%3A%20%0APesanan%20Alternatif%2FCadangan%09%3A%C2%A0(Opsional)">
+        <button className="checkout">Checkout</button>
+      </a>
     </div>
   )
 }
--- src/pages/cart/cart-item-2.jsx
+++ src/pages/cart/cart-item-2.jsx
@@ -3,19 +3,19 @@ import { useContext } from "react"
 import { ShopContext } from "../../context/shop-context"
 const convertRupiah = require('rupiah-format')
 
-export const CartItem = (props) => {
+export const CartItemFinal = (props) => {
     const {id, productName, harga} = props.data
     let rupiahBaru = convertRupiah.convert(harga)
-    const { cartItems, updateCartItemCount } = useContext(ShopContext)
+    const { cartItems } = useContext(ShopContext)
     
     return (
-        <div className="cartItem">
+        <div className="cartItemFinal">
             <div className=''>
                 <p className='product-name'>{productName}</p>
                 <p><b>{rupiahBaru}</b></p>
             </div>
-            <div className="countHandler">
-                <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} className='product-count'/>
+            <div className="countHandlerFinal">
+                <p>{cartItems[id]}x</p>
             </div>
         </div>
     )
