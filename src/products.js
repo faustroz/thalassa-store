5667a6b679295a1fb8f8579ee66dca491f259558
wahyusattriana <wahyusattriana@gmail.com>
2023-05-27 23:29:51 +0800

    reducing products

--- src/products.js
+++ src/products.js
@@ -6,26 +6,14 @@ import daluman from "./img/daluman.webp"
 export const PRODUCTS = [
     {
         id: 1,
-        productName: "Lontong Sayur",
-        harga: 10000,
-        productImage: lontongSayur
-    },
-    {
-        id: 2,
         productName: "Kolak Pisang",
         harga: 5000,
         productImage: kolakPisang,
     },
     {
-        id: 3,
+        id: 2,
         productName: "Kuud Ental",
         harga: 7000,
         productImage: kuudEntal,
-    },
-    {
-        id: 4,
-        productName: "Es Daluman",
-        harga: 5000,
-        productImage: daluman
     },
 ]
